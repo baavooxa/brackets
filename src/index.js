052acbdcab3d9f6824c007bc894490afb0d210cf
Bakur Pompaev <baavooxa@gmail.com>
2019-03-23 21:26:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < str.lenght; i++) {
+        bracketsConfig.forEach(fn);
+        var fn = function (bracket) {
+            let pairBrackets = bracket.join('');
+            if (str.indexOf(pairBackets) !== -1) {
+                str = str.replace(pairBrackets, '');
+            }
+        };
+    }
+    return !str;
+};
